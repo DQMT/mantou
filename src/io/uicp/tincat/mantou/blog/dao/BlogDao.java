16706e13502152697d1e16d04f329972db3d8d20
dqmt <daoqiemantou@gmail.com>
2017-05-21 20:19:06 +0800

    v2.0.6 删除无用代码 界面调整

--- src/io/uicp/tincat/mantou/blog/dao/BlogDao.java
+++
@@ -1,7 +0,0 @@
-package io.uicp.tincat.mantou.blog.dao;
-
-import io.uicp.tincat.mantou.blog.entity.Blog;
-
-public interface BlogDao {
-	public void saveBlog(Blog blog);
-}
--- src/io/uicp/tincat/mantou/blog/service/BlogService.java
+++
@@ -1,9 +0,0 @@
-package io.uicp.tincat.mantou.blog.service;
-
-import io.uicp.tincat.mantou.blog.entity.Blog;
-
-public interface BlogService {
-	
-	public void saveBlog(Blog blog);
-
-}
